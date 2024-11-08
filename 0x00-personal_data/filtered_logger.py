7fea99e2ea14ee1ce2980f898cc5c824e80799e7
Emmanuel Ohwoka <emmyprime2015@gmail.cocm>
2024-11-08 04:35:10 +0000

    All pycodestyle error resolved

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -11,7 +11,9 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str):
+def filter_datum(
+        fields: List[str], redaction: str, message: str, separator: str
+) -> str:
     """
     Return obfuscated messafe
     """
@@ -19,6 +21,7 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
             r'(' + '|'.join([f'{field}=[^;]+' for field in fields]) + ')',
             lambda m: m.group(0).split('=')[0] + '=' + redaction, message
     )
+
 
 class RedactingFormatter(logging.Formatter):
     """
@@ -29,27 +32,30 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPERATOR = ";"
 
-
     def __init__(self, fields: List[str]):
         """Initializws the RedactingFromatter with specific fields to redact"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
-
     def format(self, record: logging.LogRecord) -> str:
         """Formats the log record be redaction specified fields"""
         message = super().format(record)
-        return filter_datum(self.fields, self.REDACTION, message, self.SEPERATOR)
+        return filter_datum(
+                self.fields, self.REDACTION, message, self.SEPERATOR
+        )
 
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
+
 def get_logger() -> logging.Logger:
-    """Creates a logger to handle user data with sensitive information redacted."""
+    """
+    Creates a logger to handle user data with sensitive
+    information redacted.
+    """
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
-
 
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(RedactingFormatter(fields=PII_FIELDS))
@@ -58,10 +64,10 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db() ->  connection.MySQLConnection:
+def get_db() -> connection.MySQLConnection:
     """
-    Connect to the secure Holberton database using environment variables for
-    for credentials.
+    Connect to the secure Holberton database using environment
+    variables for for credentials.
     """
     username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
     password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
@@ -91,9 +97,16 @@ def main():
     """Log each row with filtered data"""
     for user in users:
         """Create a log-friendly message with sensitive data redacted"""
-        message = f"name={user['name']}; email={user['email']}; phone={user['phone']}; " \
-                  f"ssn={user['ssn']}; password={user['password']}; ip={user['ip']}; " \
-                  f"last_login={user['last_login']}; user_agent={user['user_agent']}"
+        message = (
+            f"name={user['name']}; "
+            f"email={user['email']}; "
+            f"phone={user['phone']}; "
+            f"ssn={user['ssn']}; "
+            f"password={user['password']}; "
+            f"ip={user['ip']}; "
+            f"last_login={user['last_login']}; "
+            f"user_agent={user['user_agent']}"
+        )
         logger.info(message)
 
     """Close database connection"""
