598b1efd3584f91caf9fa3c90abe618d6657cf6a
Emmanuel Ohwoka <emmyprime2015@gmail.cocm>
2024-11-13 22:40:05 +0000

    basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Basic Auth module
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """ BasicAuth class for basic authentication, inheriting from Auth. """
+    pass
+
