b98b99aee48083f5149b737e3edd9c83272b8798
Emmanuel Ohwoka <emmyprime2015@gmail.cocm>
2024-11-13 12:06:40 +0000

    Managing API authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+#!/usr/bin/env python3
+"""
+Auth module for managing API authentication
+"""
+
+from typing import List, TypeVar
+from flask import request
+
+class Auth:
+    """Class to manage the API authentication"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Checks if authentication is required for a given path
+        Returns:
+            bool: False (authentication check logic will be added later)
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Retrieves the authorization header from the request
+        Returns:
+            str: None (to be implemented)
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Retrieves the current user from the request
+        Returns:
+            TypeVar('User'): None (to be implemented)
+        """
+        return None
