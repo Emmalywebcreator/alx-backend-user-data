3246cfb288287dd9439a5a16a2b23b7d3949ba59
Emmanuel Ohwoka <emmyprime2015@gmail.cocm>
2024-11-14 07:37:13 +0000

    base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 Basic Auth module
 """
 
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -22,3 +23,19 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+        Decodes a Base64-encoded authorization header.
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except (base64.binascii.Error, UnicodeDecodeError):
+            return None
