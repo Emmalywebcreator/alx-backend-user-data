530b88bc27819a15cfff4ed101e6b56c2cd7e4bb
Emmanuel Ohwoka <emmyprime2015@gmail.cocm>
2024-11-14 11:39:42 +0000

    Verifying username and password

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 Basic Auth module
 """
 
+from typing import TypeVar
+from models.user import User
 import base64
 from api.v1.auth.auth import Auth
 
@@ -58,3 +60,26 @@ class BasicAuth(Auth):
                 decoded_base64_authorization_header.split(":", 1)
             )
         return user_email, user_password
+
+    def user_object_from_credentials(
+                self, user_email: str, user_pwd: str
+            ) -> TypeVar('User'):
+        """Retrieves User instance based on email and password."""
+
+        """Check if email and password are valid strings"""
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        """Search for user by email in database"""
+        users = User.search({"email": user_email})
+        if not users:
+            return None
+
+        """Check if password is correct for the found user"""
+        user = users[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
